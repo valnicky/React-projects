3fade299b1405b0e48d45938a44fc9db1226bb6a
Valeria <valery0007@gmail.com>
2020-08-26 00:58:26 +0200

    adding imgs and styles

--- react-app/my-app/src/AuthorQuiz.test.js
+++ react-app/my-app/src/AuthorQuiz.test.js
@@ -3,9 +3,8 @@ import { render } from '@testing-library/react';
 import AuthorQuiz from './AuthorQuiz';
 
 describe("Author Quiz", () => {
-it("renders without crashing", () => {
-const div = document.createElement("div");
-ReactDom.render(
-<AuthorQuiz />, div);
-});
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDom.render(<AuthorQuiz />, div);
+    });
 });
--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -4,8 +4,24 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikipedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  }
+];
+
+const state = {
+  tuenData: {
+  author: authors[0],
+  books: authors[0].books
+}}
+
+
 ReactDOM.render( <React.StrictMode>
-  <AuthorQuiz />
+  <AuthorQuiz {...state} />
 </React.StrictMode>,
 document.getElementById('root')
 );
