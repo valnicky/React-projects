131fe74171d3283a39eeebbc24302323a4049200
Valeria <valery0007@gmail.com>
2020-08-26 15:06:36 +0200

    authors and books

--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -9,15 +9,55 @@ const authors = [
     name: 'Mark Twain',
     imageUrl: 'images/authors/marktwain.jpg',
     imageSource: 'Wikipedia Commons',
-    books: ['The Adventures of Huckleberry Finn']
+    books: ['The Adventures of Huckleberry Finn',
+    'Life on the Mississippi',
+    'Roughing It'
+    ]
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    boobs: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: [' The Shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
   }
 ];
 
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function(p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  
+}
+
 const state = {
-  tuenData: {
-  author: authors[0],
-  books: authors[0].books
-}}
+  turnData: getTurnData(authors)
+  
+}
 
 
 ReactDOM.render( <React.StrictMode>
