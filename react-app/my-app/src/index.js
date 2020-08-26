b090c6b0faa90db3020806ff57bc2fa7a566b816
Valeria <valery0007@gmail.com>
2020-08-26 16:12:22 +0200

    npm underscore

--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
   {
@@ -51,12 +52,17 @@ function getTurnData(authors) {
   const allBooks = authors.reduce(function(p, c, i) {
     return p.concat(c.books);
   }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
   
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) => author.books.some((title) => title === answer))
+  }
 }
 
 const state = {
-  turnData: getTurnData(authors)
-  
+  turnData: getTurnData(authors) 
 }
 
 
