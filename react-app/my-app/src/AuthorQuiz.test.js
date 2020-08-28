86a140bb0cc1d1a563fcaefe8e1e9fc746b2359b
Valeria <valery0007@gmail.com>
2020-08-28 16:12:59 +0200

    adding prop-types and enzyme

--- react-app/my-app/src/AuthorQuiz.test.js
+++ react-app/my-app/src/AuthorQuiz.test.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AuthorQuiz from './AuthorQuiz';
 
+const state = {
+    turnData: {
+        books: ['The Shining', 'IT', 'David Coperfield', 'A Tale of Two Cities', Hamlet'' 
+        ],
+        author: {
+            name: 'Charles Dickens',
+            imageUrl: 'images/authors/charlesdickens.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['David Copperfield', 'A Tale of Two Cities']
+        },
+    },
+    hidhlight: 'none'
+}
+
+
+
+
 describe("Author Quiz", () => {
     it("renders without crashing", () => {
         const div = document.createElement("div");
-        ReactDom.render(<AuthorQuiz />, div);
+        ReactDom.render(<AuthorQuiz {...state} onAnswerSelected={()=> {}}/>, div);
     });
 });
--- react-app/my-app/src/index.js
+++ react-app/my-app/src/index.js
@@ -3,76 +3,77 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
-import {shuffle, sample} from 'underscore';
+import {shuffle, sample, some} from 'underscore';
 
 const authors = [
-  {
-    name: 'Mark Twain',
-    imageUrl: 'images/authors/marktwain.jpg',
-    imageSource: 'Wikipedia Commons',
-    books: ['The Adventures of Huckleberry Finn',
-    'Life on the Mississippi',
-    'Roughing It'
-    ]
-  },
-  {
-    name: 'Joseph Conrad',
-    imageUrl: 'images/authors/josephconrad.png',
-    boobs: ['Heart of Darkness']
-  },
-  {
-    name: 'J.K. Rowling',
-    imageUrl: 'images/authors/jkrowling.jpg',
-    imageSource: 'Wikimedia Commons',
-    imageAttribution: 'Daniel Ogren',
-    books: ['Harry Potter and the Sorcerers Stone']
-  },
-  {
-    name: 'Stephen King',
-    imageUrl: 'images/authors/stephenking.jpg',
-    imageSource: 'Wikimedia Commons',
-    imageAttribution: 'Pinguino',
-    books: [' The Shining', 'IT']
-  },
-  {
-    name: 'Charles Dickens',
-    imageUrl: 'images/authors/charlesdickens.jpg',
-    imageSource: 'Wikimedia Commons',
-    books: ['David Copperfield', 'A Tale of Two Cities']
-  },
-  {
-    name: 'William Shakespeare',
-    imageUrl: 'images/authors/williamshakespeare.jpg',
-    imageSource: 'Wikimedia Commons',
-    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
-  }
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikipedia Commons',
+        books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi', 'Roughing It']
+    }, {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.png',
+        boobs: ['Heart of Darkness']
+    }, {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    }, {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: [' The Shining', 'IT']
+    }, {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesdickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    }, {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/williamshakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
 ];
 
 function getTurnData(authors) {
-  const allBooks = authors.reduce(function(p, c, i) {
-    return p.concat(c.books);
-  }, []);
-  const fourRandomBooks = shuffle(allBooks).slice(0,4);
-  const answer = sample(fourRandomBooks);
-  
-  return {
-    books: fourRandomBooks,
-    author: authors.find((author) => author.books.some((title) => title === answer))
-  }
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => author.books.some((title) => title === answer))
+    }
 }
 
 const state = {
-  turnData: getTurnData(authors) 
+    turnData: getTurnData(authors),
+    highlight: 'wrong'
 }
 
+function onAnswerSelected (answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight= isCorrect ? 'correct' : 'wrong';
+  render();
+} 
 
-ReactDOM.render( <React.StrictMode>
-  <AuthorQuiz {...state} />
-</React.StrictMode>,
-document.getElementById('root')
-);
+function render() {
+ReactDOM.render(
+    <React.StrictMode>
+    <AuthorQuiz {...state} onAnswerSelected = {onAnswerSelected}/>
+</React.StrictMode>, document.getElementById('root'));
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister() to register() below. Note this comes with some pitfalls. Learn
+// more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
